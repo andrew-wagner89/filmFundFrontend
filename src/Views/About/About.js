ddf7e4ae810f3b5926f7d06b38f12f1f56a07de4
thomasverdi <thomasverdi5@gmail.com>
2017-02-27 22:53:31 -0500

    removed typo "such as such as" and changed other text on About page

--- src/Views/About/About.js
+++ src/Views/About/About.js
@@ -20,7 +20,7 @@ export class About extends React.Component {
                             <PanelGroup>
                                 <Panel className="black" header={<PageHeader>About the Film Fund</PageHeader>}>
                                     <PageHeader><small>
-                                        The Film Fund is an alternative to film industry funding platforms such as screenwriting, time-intensive grant applications, and crowdfunding campaigns via websites such as such as Kickstarter, Indiegogo, or Seed & Spark. We want to level the funding field and take the skewed advantages out of independent film.
+                                        The Film Fund is an alternative to film industry funding platforms such as screenwriting, time-intensive grant applications, and crowdfunding campaigns found on websites such as Kickstarter, Indiegogo, or Seed & Spark. We want to level the funding field and take the skewed advantages out of independent film.
                                         <br/>
                                         See our FAQ <a href="#faq">here</a>
                                     </small></PageHeader>
