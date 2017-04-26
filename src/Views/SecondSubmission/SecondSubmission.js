08b2df7f5af0fadb81e7aeefe1b1f45f0da9e799
andrew <andrew@localhost.localdomain>
2017-04-25 20:20:33 -0400

    altered site for competition end

--- src/Views/SecondSubmission/SecondSubmission.js
+++ src/Views/SecondSubmission/SecondSubmission.js
@@ -3,9 +3,7 @@
  */
 import React, { PropTypes as T } from 'react'
 import {PageHeader, Panel, PanelGroup, Col, Row, Grid, FormGroup, HelpBlock,FormControl, Button} from 'react-bootstrap'
-import StripeCheckout from 'react-stripe-checkout'
 import logo from '../../Images/facebook_logo.png'
-import stripeLogo from '../../Images/powered_by_stripe.png'
 import './SecondSubmission.css'
 
 export class SecondSubmission extends React.Component {
@@ -27,77 +25,11 @@ export class SecondSubmission extends React.Component {
             this.context.router.push('/home')
         }
     }
-    handleChangeIdea(e) {
-        this.setState({ value: e.target.value })
-    }
-    handleChangeName(e) {
-        this.setState({ name: e.target.value })
-    }
-    handleChangeEmail(e) {
-        this.setState({ email: e.target.value })
+    returnToMain(){
+        localStorage.setItem('returnedFromSubmit', 'true')
+        this.context.router.push('/home')
     }
 
-    getValidationStateIdea() {
-        const length = this.state.value.length
-        if (length > 200) return 'error'
-        else if (length > 0) return 'success'
-        else return 'error'
-    }
-    getValidationStateName() {
-        const length = this.state.name.length
-        if (length === 0) return 'error'
-        else return 'success'
-    }
-    getValidationStateEmail() {
-        const length = this.state.email.length
-        if (length === 0) return 'error'
-        else return 'success'
-    }
-    submitIdea(token){
-        const webTaskData = new FormData()
-        webTaskData.append('stripeToken', token.id)
-        console.log(JSON.stringify(token.id))
-        fetch('https://wt-5ff77dc3c93e8fcd7c00c61fe5c491f2-0.run.webtask.io/stripecharge?stripeToken=' + token.id + '&email=' + this.state.email,{
-            method: 'GET',
-        }).then(res => {
-            if (res.ok) {
-                res.text().then(text => {
-                    console.log(text)
-                    const dataToSend = new FormData();
-                    dataToSend.append('Email', this.state.email)
-                    dataToSend.append('Idea', this.state.value)
-                    dataToSend.append('Submitter', this.state.name)
-                    dataToSend.append('Token', JSON.stringify(token))
-                    fetch('https://script.google.com/macros/s/AKfycbz9fDIv7bhBWb8nTWrkg4-cKjIQ3oG9t0BgbLGnZQOorxR52UOd/exec',
-                        {
-                            method: 'POST',
-                            body: dataToSend
-                        }
-                    ).then((res) => {
-                        if (res.ok)
-                        {
-                            res.json().then(res1 => {
-                                localStorage.setItem('idea', this.state.value)
-                                localStorage.setItem('name', this.state.name)
-                                localStorage.setItem('email', this.state.email)
-                                this.context.router.push('part2submit')
-                            })
-
-                        } else {
-                            alert('unable to send your idea, please email filmfund@gmail.com with your receipt and idea')
-                        }
-                    })
-                })
-
-            } else {
-                alert('unable to contact Stripe, please try again later')
-                res.text().then(text => {
-                    console.log(text)
-                })
-            }
-        })
-
-    }
     render() {
 
         return (
@@ -106,81 +38,14 @@ export class SecondSubmission extends React.Component {
                     <Row>
                             <PanelGroup>
                                 <Panel header={<PageHeader>Welcome to the Film Fund</PageHeader>}>
-                                    <PageHeader><small>Wasn't that easy? Enter your sentence below to Round 2 for a chance to get up to $10,000 in funding to make your short film idea into a real production.
+                                    <PageHeader><small>
+                                        Thanks for entering round 1! Sadly round 2 has closed for judging. Stay tuned to your email for future updates and competitions!
                                         <br/><br/>
-                                        Contest entry fee: $25
-                                        <br/><br/>
-                                        Deadline: April 27th, 2017 at 11:59 PM
+
                                     </small></PageHeader>
-                                </Panel>
-                                <Panel header={<p>Enter Round 2 here! </p>}>
-                                    <p>Your Idea</p>
-                                    <FormGroup
-                                        controlId="Idea"
-                                        validationState={this.getValidationStateIdea()}
-                                    >
-                                        <FormControl
-                                            type="textarea"
-                                            value={this.state.value}
-                                            placeholder="Enter your idea here"
-                                            onChange={this.handleChangeIdea.bind(this)}
-                                        />
-                                        <FormControl.Feedback />
-                                        <HelpBlock>{'Current Entry length: ' + this.state.value.length + ' characters. Max allowed length: 200'}</HelpBlock>
-                                    </FormGroup>
-                                    <p>Your Name</p>
-                <Col xs={12} sm={4} smOffset={4}>
-                                    <FormGroup
-                                        controlId="Submitter"
-                                        validationState={this.getValidationStateName()}
-                                        >
-                                        <FormControl
-                                            type="textarea"
-                                            value={this.state.name}
-                                            placeholder="Enter your name here"
-                                            onChange={this.handleChangeName.bind(this)}
-                                        />
-                                    <FormControl.Feedback />
-                                        </FormGroup>
-                                        <p>Your Email</p>
-                                        <FormGroup
-                                        controlId="Email"
-                                        validationState={this.getValidationStateEmail()}
-                                        >
-                                        <FormControl
-                                            type="textarea"
-                                            value={this.state.email}
-                                            placeholder="Enter your email here"
-                                            onChange={this.handleChangeEmail.bind(this)}
-                                        />
-                                        <FormControl.Feedback />
-                                    </FormGroup>
-
-
-                                    <StripeCheckout
-                                        name="The Film Fund"
-                                        description="Pay $25 for the chance to win"
-                                        image={logo}
-                                        ComponentClass="div"
-                                        panelLabel="Pay with Credit Card"
-                                        amount={2500}
-                                        currency="USD"
-                                        stripeKey="pk_live_XkhEgLXdc6Vruep49wl4Hw8M"
-                                        email={this.state.email}
-                                        billingAddress
-                                        allowRememberMe={false}
-                                        // Note: enabling both zipCode checks and billing or shipping address will
-                                        // cause zipCheck to be pulled from billing address (set to shipping if none provided).
-                                        zipCode={false}
-                                        token={this.submitIdea.bind(this)}>
-                                        <Button className="btn btn-primary">
-                                            Pay with Credit Card
-                                        </Button>
-                                        </StripeCheckout><br/>
-                    <img src={stripeLogo}/>
-
-                                    <p>{this.state.errorMessage}</p>
-                                </Col>
+                                    <Button bsStyle="primary" onClick={this.returnToMain.bind(this)}>
+                                        Click here to return to the main page
+                                    </Button>
                                 </Panel>
                             </PanelGroup>
                     </Row>
